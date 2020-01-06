20d6bb21db9bb1c40747c40a7a0965e0f1acdab4
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 13:32:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,9 @@ def directors_totals(nds)
 end
 
 def gross_for_director(director_data)
-      {
-      
+      first_director_name = directors_database.first.values.first
+    first_director_hash = directors_database.find{ |x| x[:name] == first_director_name }
+
+    expect(gross_for_director(first_director_hash)).to eq(1357566430)
+  end
 end
