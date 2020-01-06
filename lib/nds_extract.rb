a77f833f0520d3f99abf297183f79d8a47402d91
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 13:33:06 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,15 +4,9 @@ def directors_totals(nds)
 end
 
 def gross_for_director(director_data)
-      {
-        "Stephen Spielberg"=>1357566430,
-        "Russo Brothers"=>2281002470,
-        "James Cameron"=>2571020373,
-        "Spike Lee"=>256624217,
-        "Wachowski Siblings"=>806180282,
-        "Robert Zemeckis"=>1273838385,
-        "Quentin Tarantino"=>662738268,
-        "Martin Scorsese"=>636812242,
-        "Francis Ford Coppola"=>509719258
-      }
+      first_director_name = directors_database.first.values.first
+    first_director_hash = directors_database.find{ |x| x[:name] == first_director_name }
+
+    expect(gross_for_director(first_director_hash)).to eq(1357566430)
+  end
 end
